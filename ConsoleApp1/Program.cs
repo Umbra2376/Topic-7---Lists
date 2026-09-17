ed06cb851277e148679d9a93c413933d366cc940
362572158 <362572158@2260I-L29-019>
2026-09-17 12:09:48 -0400

    hello

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,7 +12,8 @@ namespace Menu_Loop
         static void Main(string[] args)
         {
             string choice = "";
-
+            bool task1 = false;
+            bool task2 = false;
             while (choice != "q")
             {
                 Console.Clear(); // Optional
@@ -33,7 +35,12 @@ namespace Menu_Loop
                     for (int i = 0; i < 25; i++)
                     {
                         numbers.Add(randnum.Next(1, 21));
-                        Console.Write(numbers[i] + ", ");
+                    }
+                    while (task1 == false)
+                    {
+                        Console.Clear();
+                        for (int i = 0; i < numbers.Count; i++)
+                            Console.Write(numbers[i] + ", ");
                         Console.WriteLine("Now you may select what to do with this list.");
                         Console.WriteLine("1 - Sort the list");
                         Console.WriteLine("2 - Make a new list");
@@ -45,14 +52,94 @@ namespace Menu_Loop
                         Console.WriteLine("8 - Print the SUM and average value");
                         Console.WriteLine("9 - Determine the most frequntly occuring number");
                         Console.WriteLine("10 - Quit");
+                        Console.WriteLine("Please select 1-10 now");
+                        int response = Convert.ToInt32(Console.ReadLine());
+                        if (response == 1)
+                        {
+                            numbers.Sort();
+                            Console.WriteLine("The list has been sorted.");
+                        }
+                        else if (response == 2)
+                        {
+                            numbers.Clear();
+                            for (int j = 0; j < 25; j++)
+                            {
+                                numbers.Add(randnum.Next(1, 21));
+                                Console.Write(numbers[j] + ", ");
+                            }
+                            Console.WriteLine("A new list has been created.");
+                        }
+                        else if (response == 3)
+                        {
+                            Console.WriteLine("Enter a number to remove:");
+                            int numToRemove = Convert.ToInt32(Console.ReadLine());
+                            numbers.RemoveAll(x => x == numToRemove);
+                            Console.WriteLine($"All occurrences of {numToRemove} have been removed.");
+                        }
+                        else if (response == 4)
+                        {
+                            Console.WriteLine("Enter a value to add to the list:");
+                            int valueToAdd = Convert.ToInt32(Console.ReadLine());
+                            numbers.Add(valueToAdd);
+                            Console.WriteLine($"{valueToAdd} has been added to the list.");
+                        }
+                        else if (response == 5)
+                        {
+                            Console.WriteLine("Enter a number to count occurrences:");
+                            int numToCount = Convert.ToInt32(Console.ReadLine());
+                            int count = numbers.Count(x => x == numToCount);
+                            Console.WriteLine($"The number {numToCount} occurs {count} times in the list.");
+                        }
+                        else if (response == 6)
+                        {
+                            int maxValue = numbers.Max();
+                            Console.WriteLine($"The largest value in the list is {maxValue}.");
+                        }
+                        else if (response == 7)
+                        {
+                            int minValue = numbers.Min();
+                            Console.WriteLine($"The smallest value in the list is {minValue}.");
+                        }
+                        else if (response == 8)
+                        {
+                            int sum = numbers.Sum();
+                            double average = numbers.Average();
+                            Console.WriteLine($"The sum of the list is {sum} and the average is {average}.");
+                        }
+                        else if (response == 9)
+                        {
+                            var mostFrequent = numbers.GroupBy(x => x)
+                                                      .OrderByDescending(g => g.Count())
+                                                      .Select(g => g.Key)
+                                                      .First();
+                            Console.WriteLine($"The most frequently occurring number is {mostFrequent}.");
+                        }
+                        else if (response == 10)
+                        {
+                            task1 = true;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid choice, press ENTER to continue.");
+                            Console.ReadLine();
+                        }
+                        Console.WriteLine("Hit ENTER to continue.");
+                        Console.ReadLine();
                     }
-                    Console.WriteLine("Hit ENTER to continue.");
-                    Console.ReadLine();
                 }
                 else if (choice == "2")
                 {
                     // Do option 2
-                    Console.WriteLine("You chose option 2");
+                    List<string> vegetables = new List<string>();
+                    vegetables.Add("CARROT");
+                    vegetables.Add("BROCCOLI");
+                    vegetables.Add("SPINACH");
+                    vegetables.Add("KALE");
+                    vegetables.Add("PEAS");
+                    for (int i = 0; i < vegetables.Count; i++)
+                    {
+                        Console.WriteLine("1. " + vegetables[i]);
+                    }
                     Console.WriteLine("Hit ENTER to continue.");
                     Console.ReadLine();
                 }
